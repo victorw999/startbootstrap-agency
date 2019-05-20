20b7d0047815d91aa55c83e8f771fa1b3ab84d06
asus_win10 <vicwong999@gmail.com>
2019-05-20 12:42:00 -0700

    contact_captcha_4

--- contact_captcha_4/index.php
+++ contact_captcha_4/index.php
@@ -10,7 +10,7 @@ require('constant.php');
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Google reCAPTCHA v3</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
-    <script src="https://www.google.com/recaptcha/api.js?render=YOUR_RECAPTCHA_SITE_KEY"></script>
+    <script src="https://www.google.com/recaptcha/api.js?render=<?php echo SITE_KEY; ?>"></script>
     <script>
         grecaptcha.ready(function () {
             grecaptcha.execute('<?php echo SITE_KEY; ?>', { action: 'contact' }).then(function (token) {
