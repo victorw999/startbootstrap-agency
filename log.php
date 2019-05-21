e957ac2401afacae83e596743950cb5a5671dd35
victor linda_win7 <vicwong999@gmail.com>
2019-05-21 14:51:14 -0700

    log.php

--- log.php
+++ log.php
@@ -3,14 +3,13 @@
 
   function logMSG( $content ){
     $time = date('m/d/Y h:i:s a', time());
-    return $time . " : " . $content . "\n\n";
+    return $time . ": " . $content . "\n\n";
   }
-
 
   $file = "log.txt";
   $handle = fopen($file, 'a') or die('cannot open: ' .$file);
 
-  fwrite($handle, logMSG("test 1")); 
+  fwrite($handle, logMSG("test 1"));
   fclose($handle);
 
  ?>
